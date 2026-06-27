45bca62e121933a8b35e619f042346138b4412de
Utsab Niraula <niraulautsab777@gmail.com>
2026-06-27 09:17:48 +0545

    Initial commit: Journal CRUD API with Spring Boot and MongoDB

---
+++ src/main/java/com/pgs/apiProject/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.pgs.apiProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello!!!";
+    }
+}
